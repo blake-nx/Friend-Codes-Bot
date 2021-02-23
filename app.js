72623a8a744ac633292472a705ec91f92f700c9a
Blake Enloe <blakewenloe@yahoo.com>
2021-02-23 16:55:36 -0600

    added command for creating friend code

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const db = require("./db.js");
 const helpers = require("./helpers.js");
+const addFriendCode = require("./modules/addFriendCode.js");
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -17,5 +18,10 @@ client.on("ready", () => {
 client.on("message", async (msg) => {
   const args = msg.content.slice(0).trim().split(/ +/g);
   const command = args.shift();
+
+  // Add friend code
+  if (command.toLowerCase() === "addcode") {
+    addFriendCode(args.join(""), msg);
+  }
 });
 client.login(BOT_TOKEN);
---
+++ modules/addFriendCode.js
@@ -0,0 +1,32 @@
+const addFriendCode = async (friendcode, msg) => {
+  const regex = /[0-9\s]/m;
+
+  // Validate friend code
+  if (
+    friendcode.length > 15 ||
+    regex.exec(friendcode) === null ||
+    friendcode.length < 12
+  ) {
+    return msg.reply(`Invalid friend code ${friendcode}`);
+  }
+
+  // Try inserting username/friendcode into db
+  try {
+    const insertFriendCode = await db.create({
+      username: `${msg.author.username}`,
+      pogocode: friendcode,
+    });
+    return msg.reply(
+      `Friend code ${insertFriendCode.pogocode} added for ${insertFriendCode.username}`
+    );
+  } catch (e) {
+    // Return error if friendcode already exists
+    if (e.name === "SequelizeUniqueConstraintError") {
+      return msg.reply("That friendcode already exists.");
+    }
+    console.log("error adding friend code:", e);
+    return msg.reply("Something went wrong with adding a friend code.");
+  }
+};
+
+module.exports = addFriendCode;
